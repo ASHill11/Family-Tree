004db94ffba677c02522c2cbe96371c9d60497d3
Aidan <ASHill11@me.com>
2023-03-27 00:07:20 -0500

    Working our way up to nested lists

--- family-tree.py
+++ family-tree.py
@@ -4,8 +4,11 @@ def build_tree(data, parent=None):
     """Recursively build a tree from a nested list."""
     for item in data:
         if isinstance(item, list):
-            node = Node('', parent=parent)
-            build_tree(item, parent=node)
+            if isinstance(item[0], str):
+                node = Node(item[0], parent=parent)
+                build_tree(item[1:], parent=node)
+            else:
+                build_tree(item, parent=parent)
         else:
             node = Node(item, parent=parent)
 
