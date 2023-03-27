7b011ab47bd56c1abe00ae50ad5ac2dd3ca11e71
Aidan <ASHill11@me.com>
2023-03-27 00:04:02 -0500

    Trying from the beginning

--- family-tree.py
+++ family-tree.py
@@ -1,33 +1,13 @@
-# Make sure to pip install anytree
 from anytree import Node, RenderTree
 
+# Define the family members
+grandpa = Node("Grandpa")
+dad = Node("Dad", parent=grandpa)
+uncle = Node("Uncle", parent=grandpa)
+child1 = Node("Child 1", parent=dad)
+child2 = Node("Child 2", parent=dad)
+child3 = Node("Child 3", parent=uncle)
 
-def build_tree(data, parent=None):
-    """Recursively build a tree from a nested list."""
-    node_dict = {}
-    for item in data:
-        if isinstance(item, list):
-            child_key = str(item)
-            if child_key not in node_dict:
-                node = Node('', parent=parent)
-                node_dict[child_key] = node
-                build_tree(item, parent=node)
-        else:
-            child_key = str(item)
-            if child_key not in node_dict:
-                node = Node(child_key, parent=parent)
-                node_dict[child_key] = node
-
-
-def print_tree(tree):
-    """Print the tree chart using anytree's RenderTree function."""
-    for pre, _, node in RenderTree(tree):
-        print(f"{pre}{node.name}")
-
-
-# Example usage:
-data = ['Cory Davis', ['Ryan Day', ['Travis Stone', ['Robert White', ['Alan', 'Joel', 'Chris']]], 'Phillip Knowles', 'Steven Wang', 'Ivan Valdez']]
-root = Node('Ben King')
-build_tree(data, parent=root)
-print_tree(root)
-
+# Print the family tree
+for pre, _, node in RenderTree(grandpa):
+    print(f"{pre}{node.name}")
