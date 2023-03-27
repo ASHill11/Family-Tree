85d686a2e9382b811cb13c21f57522c9b5b3649a
Aidan <ASHill11@me.com>
2023-03-26 23:55:05 -0500

    Updated build_tree function to not print lists

--- family-tree.py
+++ family-tree.py
@@ -2,12 +2,16 @@
 from anytree import Node, RenderTree
 
 
-def build_tree(data, parent=None):
+def build_tree(data, parent=None, parent_name='root'):
     """Recursively build a tree from a nested list."""
     for item in data:
-        node = Node(str(item), parent=parent)
         if isinstance(item, list):
-            build_tree(item, parent=node)
+            node = Node(str(item[0]), parent=parent)
+            build_tree(item, parent=node, parent_name=str(item[0]))
+        else:
+            node = Node(str(item), parent=parent)
+        if parent_name and node.parent:
+            node.parent.name = parent_name
 
 
 def print_tree(tree):
@@ -17,8 +21,8 @@ def print_tree(tree):
 
 
 # Example usage:
-data = ['a', ['b', ['d', 'e'], 'c'], 'f']
-root = Node('root')
+data = ['Cory Davis', ['Ryan Day', ['Travis Stone', ['Robert White', ['Alan', 'Joel', 'Chris']]], 'Phillip Knowles', 'Steven Wang', 'Ivan Valdez']]
+root = Node('Ben King')
 build_tree(data, parent=root)
 print_tree(root)
 
