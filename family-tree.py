482bfcce6ec48891594805a2d10c6560f4ecd37a
Aidan <ASHill11@me.com>
2023-03-27 12:39:46 -0500

    Alright so we've actually got a program now that can parse Excel and populate a list with classes. Pretty cool.

--- family-tree.py
+++ family-tree.py
@@ -1,5 +1,8 @@
+import openpyxl
+
+
 class Person:
-    def __init__(self, parse_id, first_name, last_name, class_year, parent_1=None, parent_2=None, parent_3=None, child_1=None, child_2=None, child_3=None, child_4=None, child_5=None):
+    def __init__(self, first_name, last_name, class_year, parse_id, parent_1=None, parent_2=None, parent_3=None, child_1=None, child_2=None, child_3=None, child_4=None, child_5=None):
         self.parse_id = parse_id
         self.first_name = first_name
         self.last_name = last_name
@@ -12,3 +15,19 @@ class Person:
         self.child_3 = child_3
         self.child_4 = child_4
         self.child_5 = child_5
+
+
+def read_excel_data(filename):
+    wb = openpyxl.load_workbook(filename)
+    sheet = wb.active
+    data = []
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        person = Person(*row)
+        data.append(person)
+    return data
+
+
+people = read_excel_data('family-tree-data.xlsx')
+for person in people:
+    print(f"ID: {person.parse_id}, Name: {person.first_name} {person.last_name} Class Year: {person.class_year}")
+
