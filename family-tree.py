b5af882fd4bc13f3257e0a4421a3aa29d5974e39
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-03-28 21:57:58 -0500

    updated Person object and read_excel_data function

--- family-tree.py
+++ family-tree.py
@@ -7,19 +7,13 @@ file_path = r"C:\Users\ASHil\PycharmProjects\Family-Tree\family-tree-data.xlsx"
 
 
 class Person:
-    def __init__(self, first_name, last_name, class_year, parse_id, parent_1=None, parent_2=None, parent_3=None, child_1=None, child_2=None, child_3=None, child_4=None, child_5=None):
+    def __init__(self, first_name, last_name, class_year, parse_id, parents=[], children=[]):
         self.parse_id = parse_id
         self.first_name = first_name
         self.last_name = last_name
         self.class_year = class_year
-        self.parent_1 = parent_1
-        self.parent_2 = parent_2
-        self.parent_3 = parent_3
-        self.child_1 = child_1
-        self.child_2 = child_2
-        self.child_3 = child_3
-        self.child_4 = child_4
-        self.child_5 = child_5
+        self.parents = parents
+        self.children = children
 
 
 def read_excel_data(filename):
@@ -42,16 +36,15 @@ def read_excel_data(filename):
         first_name = worksheet.cell(row=row_num, column=2).value
         last_name = worksheet.cell(row=row_num, column=3).value
         class_year = worksheet.cell(row=row_num, column=4).value
-        parent_1 = worksheet.cell(row=row_num, column=5).value
-        parent_2 = worksheet.cell(row=row_num, column=6).value
-        parent_3 = worksheet.cell(row=row_num, column=7).value
-        child_1 = worksheet.cell(row=row_num, column=8).value
-        child_2 = worksheet.cell(row=row_num, column=9).value
-        child_3 = worksheet.cell(row=row_num, column=10).value
-        child_4 = worksheet.cell(row=row_num, column=11).value
-        child_5 = worksheet.cell(row=row_num, column=12).value
-        person = Person(id_num, first_name, last_name, class_year, parent_1, parent_2, parent_3, child_1, child_2,
-                        child_3, child_4, child_5)
+        parents = []
+        for i in [5, 6, 7]:
+            if worksheet.cell(row=row_num, column=i).value != None:
+                parents.append(worksheet.cell(row=row_num, column=i).value)
+        children = []
+        for i in [8, 9, 10, 11, 12]:
+            if worksheet.cell(row=row_num, column=i).value != None:
+                children.append(worksheet.cell(row=row_num, column=i).value)
+        person = Person(id_num, first_name, last_name, class_year, parents, children)
 
         # Add the Person object to the list
         people_list.append(person)
